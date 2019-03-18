4be1b969a0c3ab240024c3c80153c2625c631fc8
magmanex <timecrisis1995@live.com>
2019-03-18 13:30:06 +0700

    Add file ga.py

---
+++ ga.py
@@ -0,0 +1,11 @@
+# TITLE : COMPUTER-AIDED MANUFACTURING
+# COURSE NO : 261473
+# Developed By(github) : magmanex
+# Name : Arinchai Nanjaruwong
+# STUID : 580610692
+
+GA_POPSIZE 2048 #Population size
+GA_MAXITER 1648 #Maximum iterations
+GA_ELITERATE 0.10 # Elitism rate
+GA_MUTPERC 0.25 # Mutation rate
+PRINT_INTER 0 # Print status every this iterations/generations
